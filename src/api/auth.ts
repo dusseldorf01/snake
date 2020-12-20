92eacaf5f7c9b0af3670a4fd95f28f4237e37a32
Vladimir <corndev@yandex.ru>
2020-12-20 21:51:21 +0300

    Axios config, auth api calls

---
+++ src/api/auth.ts
@@ -0,0 +1,8 @@
+import api, { addPrefix, ApiParams } from '@/utils/api';
+
+const withPrefix = addPrefix('auth');
+
+export const signUp = ({ data }:ApiParams) => api.post(withPrefix('signup'), data);
+export const signIn = ({ data }:ApiParams) => api.post(withPrefix('signin'), data);
+export const signOut = () => api.post(withPrefix('logout'));
+export const getUserInfo = () => api.get(withPrefix('user'));
---
+++ src/utils/api.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+export type ApiParams = AxiosRequestConfig;
+
+const url = 'https://ya-praktikum.tech/api/v2/';
+
+export const addPrefix = (prefix: string) => (endpoint:string) => `${prefix}/${endpoint}`;
+
+const api = axios.create({
+  baseURL: url,
+  timeout: 3 * 60000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+export default api;
