9433b9513df76832079db91ecc9619efc1d4bddc
Vladimir <corndev@yandex.ru>
2021-01-18 00:29:29 +0300

    .env support for webpack added, terser plugin for minify production build, service worker added, app available in offline mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import './serviceWorkers/init';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -9,6 +10,7 @@ import rootSaga from './saga';
 import './styles/variables.css';
 import './styles/index.css';
 
+// fetch('https://ya-praktikum.tech/api/v2/auth/user', { credentials: 'same-origin' });
 const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: rootReducer,
